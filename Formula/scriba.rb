9cdd547875ead0dc7de73dfbcedd2d07d263b453
GitHub Action <action@github.com>
2026-03-29 14:43:58 +0000

    chore(formula): update to v0.22.1

--- Formula/scriba.rb
+++ Formula/scriba.rb
@@ -1,14 +1,14 @@
 class Scriba < Formula
   desc "Modern CLI tool for recording and transcribing audio using OpenAI Whisper"
   homepage "https://github.com/giovannialberto/scriba"
-  version "0.22.0"
+  version "0.22.1"
   
   if Hardware::CPU.intel?
-    url "https://github.com/giovannialberto/scriba/releases/download/v0.22.0/scriba-x86_64-apple-darwin"
-    sha256 "248333f0572b9980e8d2a7297bd943ba351331cbdff9cf854c69bb5b4f384f9c"
+    url "https://github.com/giovannialberto/scriba/releases/download/v0.22.1/scriba-x86_64-apple-darwin"
+    sha256 "1b649e19b4cefdb27a822e82e7260cfa48737cb7b435196523c8784869c85ec4"
   else
-    url "https://github.com/giovannialberto/scriba/releases/download/v0.22.0/scriba-aarch64-apple-darwin"
-    sha256 "b7aea5106de1ee35fa2f90f3dfeca803392a1bbe838a85d1302ff8ea8855e883"
+    url "https://github.com/giovannialberto/scriba/releases/download/v0.22.1/scriba-aarch64-apple-darwin"
+    sha256 "da9b152be9aa80777913ed3c8f4d13031a08a2a63f51dd0d7180ac4e2234eae4"
   end
   
   def install
